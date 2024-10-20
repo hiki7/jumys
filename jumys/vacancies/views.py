9d8f2d1c3e639321d5c25e2858a6921d679eeaaf
Islam <kogsaitama@gmail.com>
2024-10-20 16:07:05 +0500

    change merge view

--- jumys/vacancies/views.py
+++ jumys/vacancies/views.py
@@ -1,7 +1,11 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render
+
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
+from django.db.models import Q
+
 from .models import Vacancy, HiddenVacancies, Company
 
 class VacancyListView(ListView):
@@ -48,16 +52,6 @@ class CompanyListView(ListView):
     template_name = 'vacancies/company_list.html'
     context_object_name = 'companies'
 
-from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
-from django.db.models import Q
-
-from .models import Vacancy
-
-
-from django.db.models import Q
-from django.views.generic import ListView
-from .models import Vacancy
 
 class HomePageView(ListView):
     model = Vacancy
@@ -71,13 +65,12 @@ class HomePageView(ListView):
 
         if search_query:
             queryset = queryset.filter(
-                Q(position_name__title__icontains=search_query) |  # Adjust to correct field name
-                Q(salary_start__icontains=search_query) |  # Adjust based on your model fields
-                Q(company__name__icontains=search_query) |  # Adjust based on your model fields
-                Q(location__name__icontains=search_query) |  # Adjust based on your model fields
-                Q(employment_type__name__icontains=search_query) |  # Adjust based on your model fields
-                Q(technology__name__icontains=search_query)  # Adjust based on your model fields
+                Q(position_name__title__icontains=search_query) |  
+                Q(salary_start__icontains=search_query) |  
+                Q(company__name__icontains=search_query) |  
+                Q(location__name__icontains=search_query) |  
+                Q(employment_type__name__icontains=search_query) |  
+                Q(technology__name__icontains=search_query)  
             )
 
-        # Order by a specific field (for example, by position name)
-        return queryset.order_by('-salary_start')  # Replace with your desired field for ordering
+        return queryset.order_by('-salary_start')  
