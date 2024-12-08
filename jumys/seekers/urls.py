67fe56bd59d4237b407af900e8b23e97df3c970a
hiki7 <rsaliyev03@gmail.com>
2024-12-08 15:53:07 +0500

    change: the naming convention for endpoints in the seeker's profile

--- jumys/seekers/urls.py
+++ jumys/seekers/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('profile/', UserProfileView.as_view(), name='user_profile'),
     path('apply/<int:vacancy_id>/', ApplyToVacancyView.as_view(), name='apply_to_vacancy'),
     path('bookmark/<int:vacancy_id>/', BookmarkVacancyView.as_view(), name='bookmark_vacancy'),
-    path('abilities/', AbilityListCreateView.as_view(), name='ability_list_create'),
-    path('work-experience/', WorkExperienceListCreateView.as_view(), name='work_experience_list_create'),
+    path('profile/abilities/', AbilityListCreateView.as_view(), name='ability_list_create'),
+    path('profile/work-experience/', WorkExperienceListCreateView.as_view(), name='work_experience_list_create'),
 ]
