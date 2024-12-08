2fd050e1a0616c0c9b1f8521c112371219746b10
hiki7 <rsaliyev03@gmail.com>
2024-12-08 17:10:36 +0500

    delete: applied_vacancies and bookmarked_vacancies fields in the user_profile model

--- jumys/seekers/models.py
+++ jumys/seekers/models.py
@@ -29,8 +29,6 @@ class UserProfile(models.Model):
     links = models.TextField(blank=True, null=True)
     resume = models.FileField(upload_to='resumes/', blank=True, null=True)
     abilities = models.ManyToManyField(Ability, blank=True, related_name='users')
-    bookmarked_vacancies = models.ManyToManyField(Vacancy, blank=True, related_name='bookmarked_by')
-    applied_vacancies = models.ManyToManyField('Application', blank=True, related_name='applicants')
 
     def __str__(self):
         return self.user.email
